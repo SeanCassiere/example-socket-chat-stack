8bd3b591249672fbaa066991dd5929ff8b69ce22
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-20 18:43:19 +0530

    feat: added server startup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,34 @@
-import { CONSTANTS } from "@seanc-sockets-stack/common";
+import "reflect-metadata";
+import cors, { CorsOptions } from "cors";
+import { createServer } from "http";
+import express from "express";
+import { Server } from "socket.io";
 
-console.log("Hello from server");
-console.log("From common package", CONSTANTS);
+import socket from "#root/conn/socket";
+import { log } from "#root/utils/logger";
+
+const PORT = process.env.PORT || 4000;
+
+const corsOptions: CorsOptions = {
+	origin: (_, cb) => cb(null, true),
+	credentials: true,
+};
+
+// Server setup
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+	cors: { ...corsOptions },
+});
+
+// Express setup
+app.use(cors({ ...corsOptions }));
+
+app.get("/", (_, res) => res.send("<h2>Hello World</h2>"));
+
+httpServer.listen(PORT, () => {
+	log.info(`🚀 Server is listening on port ${PORT} 🚀`);
+
+	socket({ io });
+});
