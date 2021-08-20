cfd333e92473ac430897d334325f9cf19dfa8227
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-20 18:53:27 +0530

    feat: added swagger distribution

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,17 @@
 import "reflect-metadata";
 import cors, { CorsOptions } from "cors";
 import { createServer } from "http";
+import cookieParser from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
 import express from "express";
 import { Server } from "socket.io";
 
 import socket from "#root/conn/socket";
+import swaggerDocument from "#root/swagger.json";
 import { log } from "#root/utils/logger";
 
 const PORT = process.env.PORT || 4000;
+const COOKIE_SECRET = process.env.COOKIE_SECRET || "cookie_secret";
 
 const corsOptions: CorsOptions = {
 	origin: (_, cb) => cb(null, true),
@@ -24,6 +28,14 @@ const io = new Server(httpServer, {
 
 // Express setup
 app.use(cors({ ...corsOptions }));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(express.json());
+
+// Swagger distribution
+app.get("/docs/swagger.json", (_, res) => {
+	res.send(swaggerDocument);
+});
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (_, res) => res.send("<h2>Hello World</h2>"));
 
