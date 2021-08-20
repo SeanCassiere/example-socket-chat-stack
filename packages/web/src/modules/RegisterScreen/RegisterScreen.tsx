b53003b7c3c80f8bdc3f6db2f44538fa02eea33e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-20 22:44:13 +0530

    feat: add register screen

--- packages/web/src/modules/RegisterScreen/RegisterScreen.tsx
+++ packages/web/src/modules/RegisterScreen/RegisterScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Form, Container, FloatingLabel, Row, Col } from "react-bootstrap";
 
-export const LoginScreen = () => {
+export const RegisterScreen = () => {
 	function handleSubmit(e: React.SyntheticEvent) {
 		e.preventDefault();
 	}
@@ -10,12 +10,24 @@ export const LoginScreen = () => {
 		<Container className='mt-5'>
 			<Row className='justify-content-md-center align-items-md-center mb-3'>
 				<Col xs lg='6'>
-					<h2>Login</h2>
+					<h2>Register</h2>
 				</Col>
 			</Row>
 			<Row className='justify-content-md-center align-items-md-center'>
 				<Col xs lg='6'>
 					<Form onSubmit={handleSubmit}>
+						<Form.Group className='mb-3' controlId='formBasicFirstName'>
+							<FloatingLabel controlId='floatingFirstName' label='First Name' className='mb-3'>
+								<Form.Control type='text' placeholder='John' />
+							</FloatingLabel>
+						</Form.Group>
+
+						<Form.Group className='mb-3' controlId='formBasicLastName'>
+							<FloatingLabel controlId='floatingLastName' label='Last Name' className='mb-3'>
+								<Form.Control type='text' placeholder='Doe' />
+							</FloatingLabel>
+						</Form.Group>
+
 						<Form.Group className='mb-3' controlId='formBasicEmail'>
 							<FloatingLabel controlId='floatingUsername' label='Username' className='mb-3'>
 								<Form.Control type='text' placeholder='Your username' />
@@ -29,7 +41,7 @@ export const LoginScreen = () => {
 						</Form.Group>
 
 						<Button variant='primary' type='submit'>
-							Login
+							Register
 						</Button>
 					</Form>
 				</Col>
@@ -38,4 +50,4 @@ export const LoginScreen = () => {
 	);
 };
 
-export default LoginScreen;
+export default RegisterScreen;
--- packages/web/src/routes/Routes.tsx
+++ packages/web/src/routes/Routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { AuthRoute } from "./AuthRoute";
 import Navbar from "#root/shared/component/Navbar/Navbar";
 import LoginScreen from "#root/modules/LoginScreen/LoginScreen";
+import RegisterScreen from "#root/modules/RegisterScreen/RegisterScreen";
 
 const Routes = () => {
 	return (
@@ -13,6 +14,7 @@ const Routes = () => {
 				<Switch>
 					<Route path='/' component={LoginScreen} exact />
 					<Route path='/login' component={LoginScreen} exact />
+					<Route path='/register' component={RegisterScreen} exact />
 					<AuthRoute path='/hello' component={LoginScreen} exact />
 				</Switch>
 			</BrowserRouter>
