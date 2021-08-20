7ae372cd88f37e0d66fafddd1a3ccb37592e4f8d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-20 20:45:09 +0530

    feat: import missing utils

---
+++ packages/server/src/utils/generateToken.ts
@@ -0,0 +1,20 @@
+import jwt, { Secret } from "jsonwebtoken";
+
+import { GeneratedTokenInterface } from "#root/interfaces/jwtTokenInterfaces";
+import { environmentVariables } from "./env";
+
+const JWT_SECRET: Secret = environmentVariables.JWT_SECRET || "dev_jwt_secret";
+const REFRESH_JWT_SECRET: Secret = environmentVariables.REFRESH_JWT_SECRET || "dev_refresh_jwt_secret";
+
+type TokenTypes = "ACCESS_TOKEN" | "REFRESH_TOKEN";
+
+export const generateToken = (type: TokenTypes, { id, email }: GeneratedTokenInterface, mins: number) => {
+	let jwtPayload: GeneratedTokenInterface = { id };
+	const secret = type === "REFRESH_TOKEN" ? REFRESH_JWT_SECRET : JWT_SECRET;
+
+	if (email) jwtPayload = { ...jwtPayload, email };
+
+	return jwt.sign({ ...jwtPayload }, secret, {
+		expiresIn: `${mins}m`,
+	});
+};
