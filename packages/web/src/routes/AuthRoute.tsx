e6fa074a9f061e099779004cf7b5ab8f976235dd
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-21 00:03:41 +0530

    fix: redirect from login when auth route

--- packages/web/src/routes/AuthRoute.tsx
+++ packages/web/src/routes/AuthRoute.tsx
@@ -1,32 +1,21 @@
 import React from "react";
-import { RouteComponentProps, Route, RouteProps } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Route, RouteProps, Redirect } from "react-router-dom";
 
-export const AuthRoute = (props: RouteProps) => {
-	const renderRoute = (routeProps: RouteComponentProps<{}>) => {
-		/**
-		 * See this BenAwal ABB Clone Repo: https://github1s.com/benawad/fullstack-graphql-airbnb-clone/blob/HEAD/packages/controller/src/modules/auth/AuthRoute.tsx#L13-L16
-		 */
-		// if (!data || data.loading) {
-		//   // loading screen
-		//   return null;
-		// }
+import { selectAuthUserState } from "#root/shared/redux/store";
 
-		// if (!data.me) {
-		//   // user not logged in
-		//   return (
-		//     <Redirect
-		//       to={{
-		//         pathname: "/login",
-		//         state: { next: routeProps.location.pathname }
-		//       }}
-		//     />
-		//   );
-		// }
+export const AuthRoute = (routeProps: RouteProps) => {
+	const { isLoggedIn } = useSelector(selectAuthUserState);
+	if (!isLoggedIn) {
+		return (
+			<Redirect
+				to={{
+					pathname: "/login",
+					state: { next: routeProps?.location?.pathname },
+				}}
+			/>
+		);
+	}
 
-		const Component = props.component as any;
-
-		return <Component {...routeProps} />;
-	};
-
-	return <Route render={renderRoute} />;
+	return <Route {...routeProps} />;
 };
