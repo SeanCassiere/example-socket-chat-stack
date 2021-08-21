2fa1d5cae3b33111bd1a0404668ac91547ad4825
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-08-21 12:36:54 +0530

    chore: add loader component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import "#root/App.css";
 
 import Routes from "./routes/Routes";
+import { AppDispatch, selectAuthUserState } from "./shared/redux/store";
 import { userFetchRefreshedAccessTokenThunk } from "./shared/redux/thunks/authUser.thunks";
+import Loader from "./shared/component/Loader/Loader";
 
 const App = () => {
-	const dispatch = useDispatch();
+	const dispatch = useDispatch<AppDispatch>();
+	const { isAuthenticating } = useSelector(selectAuthUserState);
 
 	// auto login if cookie available
 	useEffect(() => {
 		dispatch(userFetchRefreshedAccessTokenThunk());
 	}, [dispatch]);
+
+	if (isAuthenticating) return <Loader />;
 
 	return (
 		<>
